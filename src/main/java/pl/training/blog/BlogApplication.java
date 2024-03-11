c7c1db74ef11c813f9521e73a3ad9992adff184e
landrzejewski <landrzejewski.poczta@gmail.com>
2024-03-11 14:01:07 +0100

    update

--- src/main/java/pl/training/blog/BlogApplication.java
+++ src/main/java/pl/training/blog/BlogApplication.java
@@ -1,13 +1,8 @@
 package pl.training.blog;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class BlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlogApplication.class, args);
     }
 
 }
