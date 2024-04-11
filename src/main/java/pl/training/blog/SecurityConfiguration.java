d353f15b20cf619c82ce24b1b231cf1ccc1bfb27
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-04-11 15:55:20 +0200

    update

---
+++ src/main/java/pl/training/blog/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package pl.training.blog;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        return username -> User.withUsername("admin")
+                .roles("ADMIN")
+                .password(passwordEncoder.encode("123"))
+                .build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(config -> config
+                        .requestMatchers(GET, "/categories/**").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(GET, "/articles/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .httpBasic(withDefaults())
+                .build();
+    }
+
+}
--- src/main/java/pl/training/blog/common/web/GlobalExceptionHandler.java
+++ src/main/java/pl/training/blog/common/web/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package pl.training.blog.common.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import pl.training.blog.application.ArticleNotFoundException;
 import pl.training.blog.common.validation.ValidationExceptionMapper;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice(annotations = RestController.class)
 @RequiredArgsConstructor
@@ -19,6 +23,12 @@ public class GlobalExceptionHandler {
         var validationErrors = exceptionMapper.toValidationErrors(exception);
         return ResponseEntity.badRequest()
                 .body(new ExceptionDto(validationErrors));
+    }
+
+    @ExceptionHandler(ArticleNotFoundException.class)
+    public ResponseEntity<ExceptionDto> onArticleNotFoundException(ArticleNotFoundException exception) {
+        return ResponseEntity.status(NOT_FOUND)
+                .body(new ExceptionDto("Article not found"));
     }
 
 }
