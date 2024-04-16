b6614945a7e434f3cfe811729d2d8cc432b3b145
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-04-16 14:29:29 +0200

    update

---
+++ src/main/java/pl/training/blog/adapters/infrastructure/persistence/jpa/ArticleEntity.java
@@ -0,0 +1,45 @@
+package pl.training.blog.adapters.infrastructure.persistence.jpa;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import static jakarta.persistence.CascadeType.*;
+
+@NamedEntityGraph(name = ArticleEntity.WITH_TAGS, attributeNodes = {@NamedAttributeNode("tags")})
+@Table(name = "Articles", indexes = @Index(name = "article_category", columnList = "category"))
+@Entity(name = "Article")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class ArticleEntity {
+
+    public static final String WITH_TAGS = "ArticleEntity.withTags";
+
+    @Id
+    private UUID id;
+    @Column(nullable = false, length = 200)
+    private String title;
+    private String author;
+    @Lob
+    @Column(length = 1024)
+    private String content;
+    private String category;
+    private String status;
+    @ManyToMany(cascade = {PERSIST, MERGE})
+    private Set<TagEntity> tags;
+    private Instant created;
+    private Instant published;
+    private int likes;
+    private int dislikes;
+    @JoinColumn(name = "article_id")
+    @OneToMany(cascade = ALL)
+    private List<CommentEntity> comments;
+
+}
---
+++ src/main/java/pl/training/blog/adapters/infrastructure/persistence/jpa/SpringDataArticleRepositoryAdapter.java
@@ -0,0 +1,76 @@
+package pl.training.blog.adapters.infrastructure.persistence.jpa;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import pl.training.blog.application.ArticleView;
+import pl.training.blog.common.PageDefinition;
+import pl.training.blog.common.ResultPage;
+import pl.training.blog.domain.Article;
+import pl.training.blog.domain.ArticleCategory;
+import pl.training.blog.domain.Tag;
+import pl.training.blog.ports.infrastructure.ArticleRepository;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@Primary
+@Transactional
+@Repository
+@RequiredArgsConstructor
+public class SpringDataArticleRepositoryAdapter implements ArticleRepository {
+
+    private final SpringDataArticleRepository articleRepository;
+    private final SpringDataArticleMapper articleMapper;
+
+    @Override
+    public Optional<Article> findById(UUID id) {
+        return articleRepository.findById(id)
+                .map(articleMapper::toDomain);
+    }
+
+    @Override
+    public ResultPage<ArticleView> findByCategory(ArticleCategory category, PageDefinition pageDefinition) {
+        var page = articleMapper.toEntity(pageDefinition);
+        var categoryName = articleMapper.toEntity(category);
+        var result = articleRepository.findByCategory(categoryName, page);
+        return articleMapper.toDomain(result);
+    }
+
+    @Override
+    public ResultPage<ArticleView> findByTags(Set<Tag> tags, PageDefinition pageDefinition) {
+        var page = articleMapper.toEntity(pageDefinition);
+        var tagNames = articleMapper.toEntity(tags);
+        var result = articleRepository.findByTags(tagNames, tagNames.size(), page);
+        return articleMapper.toDomain(result);
+    }
+
+    @Override
+    public ResultPage<ArticleView> findByCategoryAndTags(ArticleCategory category, Set<Tag> tags, PageDefinition pageDefinition) {
+        var page = articleMapper.toEntity(pageDefinition);
+        var categoryName = articleMapper.toEntity(category);
+        var tagNames = articleMapper.toEntity(tags);
+        var result = articleRepository.findByCategoryAndTags(categoryName, tagNames, tagNames.size(), page);
+        return articleMapper.toDomain(result);
+    }
+
+    @Override
+    public Article save(Article article) {
+        var articleEntity = articleMapper.toEntity(article);
+        var savedArticleEntity = articleRepository.save(articleEntity);
+        return articleMapper.toDomain(savedArticleEntity);
+    }
+
+    @Override
+    public void deleteById(UUID articleId) {
+        articleRepository.deleteById(articleId);
+    }
+
+    @Override
+    public boolean existsByTitle(String title) {
+        return articleRepository.existsByTitle(title);
+    }
+
+}
