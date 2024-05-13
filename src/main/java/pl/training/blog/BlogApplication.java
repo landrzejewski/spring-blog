86965e1fda1d03cec30b57b96c0684f4b1c97f3a
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-05-13 12:09:06 +0200

    base

--- src/main/java/pl/training/blog/BlogApplication.java
+++ src/main/java/pl/training/blog/BlogApplication.java
@@ -2,8 +2,8 @@ package pl.training.blog;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pl.training.blog.application.ArticleTemplate;
-import pl.training.blog.common.PageDefinition;
 import pl.training.blog.ports.api.ArticleAuthorActionsApi;
 import pl.training.blog.ports.api.ArticleSearchApi;
 
@@ -14,14 +14,14 @@ import static pl.training.blog.domain.ArticleCategory.IT;
 public class BlogApplication {
 
     public static void main(String[] args) {
-        ArticleAuthorActionsApi authorActions = null;
-        ArticleSearchApi search = null;
+        try (var container = new AnnotationConfigApplicationContext(BlogConfiguration.class)) {
+            var authorActions = container.getBean(ArticleAuthorActionsApi.class);
+            var search = container.getBean(ArticleSearchApi.class);
 
-        var article = new ArticleTemplate("Test", "Jan Kowalski", "",  IT);
-        var id = authorActions.create(article);
-        log.info(search.findByUid(id).toString());
-        search.findByCategory(IT, new PageDefinition(0, 10));
-        search.findByCategory(IT, new PageDefinition(0, 10));
+            var article = new ArticleTemplate("Test", "Jan Kowalski", "",  IT);
+            var id = authorActions.create(article);
+            log.info(search.findByUid(id).toString());
+        }
     }
 
 }
