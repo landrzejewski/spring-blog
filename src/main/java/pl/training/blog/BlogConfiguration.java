0e79275bc964404aa679769a9e9e8cbd5a7f0872
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-09-02 11:33:35 +0200

    config

--- src/main/java/pl/training/blog/BlogConfiguration.java
+++ src/main/java/pl/training/blog/BlogConfiguration.java
@@ -1,9 +1,34 @@
 package pl.training.blog;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import pl.training.blog.application.ArticleAuthorActions;
+import pl.training.blog.application.ArticleReaderActions;
+import pl.training.blog.application.ArticleSearch;
+import pl.training.blog.ports.api.ArticleAuthorActionsApi;
+import pl.training.blog.ports.api.ArticleReaderActionsApi;
+import pl.training.blog.ports.api.ArticleSearchApi;
+import pl.training.blog.ports.infrastructure.ArticleRepository;
+import pl.training.blog.ports.infrastructure.EventsEmitter;
 
 @ComponentScan
 @Configuration
 public class BlogConfiguration {
+
+    @Bean
+    public ArticleAuthorActionsApi articleAuthorActions(ArticleRepository articleRepository) {
+        return new ArticleAuthorActions(articleRepository);
+    }
+
+    @Bean
+    public ArticleReaderActionsApi articleReaderActions(ArticleRepository articleRepository, EventsEmitter eventsEmitter) {
+        return new ArticleReaderActions(articleRepository, eventsEmitter);
+    }
+
+    @Bean
+    public ArticleSearchApi articleSearch(ArticleRepository articleRepository) {
+        return new ArticleSearch(articleRepository);
+    }
+
 }
