b237a154e3e986fe8e871ecfebf6c18053cf1277
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-09-03 12:22:27 +0200

    config

--- src/main/java/pl/training/blog/BlogApplication.java
+++ src/main/java/pl/training/blog/BlogApplication.java
@@ -2,27 +2,46 @@ package pl.training.blog;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pl.training.blog.application.ArticleTemplate;
+import pl.training.blog.common.PageDefinition;
+import pl.training.blog.domain.ArticleUpdate;
+import pl.training.blog.domain.Tag;
 import pl.training.blog.ports.api.ArticleAuthorActionsApi;
+import pl.training.blog.ports.api.ArticleReaderActionsApi;
 import pl.training.blog.ports.api.ArticleSearchApi;
+
+import java.util.Set;
 
 import static pl.training.blog.domain.ArticleCategory.IT;
 
-@RequiredArgsConstructor
+@SpringBootApplication
 @Log
-public class BlogApplication {
+@RequiredArgsConstructor
+public class BlogApplication implements ApplicationRunner {
+
+    private final ArticleAuthorActionsApi authorActions;
+    private final ArticleReaderActionsApi readerActions;
+    private final ArticleSearchApi search;
 
     public static void main(String[] args) {
-            try (var container = new AnnotationConfigApplicationContext(BlogConfiguration.class)) {
-                var authorActions = container.getBean(ArticleAuthorActionsApi.class);
-                var search = container.getBean(ArticleSearchApi.class);
+        SpringApplication.run(BlogApplication.class, args);
+    }
 
-                var article = new ArticleTemplate("Test", "Jan Kowalski", "",  IT);
-                var id = authorActions.create(article);
-                log.info(search.findByUid(id).toString());
-                // log.info(search.findByUid(id).toString());
-            }
-     }
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        var article = new ArticleTemplate("Test", "Jan Kowalski", "",  IT);
+        var id = authorActions.create(article);
+        readerActions.like(id);
+        var tags = Set.of(new Tag("training"));
+        var articleUpdate = new ArticleUpdate(null, null, tags);
+        authorActions.update(id, articleUpdate);
+        log.info(search.findByUid(id).toString());
+        log.info(search.findByCategory(IT, new PageDefinition(0, 10)).toString());
+        log.info(search.findByTags(tags, new PageDefinition(0, 10)).toString());
+    }
 
 }
---
+++ src/main/java/pl/training/blog/adapters/infrastructure/persistence/jpa/SpringDataRepositoryAdapter.java
@@ -0,0 +1,76 @@
+package pl.training.blog.adapters.infrastructure.persistence.jpa;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import pl.training.blog.application.ArticleView;
+import pl.training.blog.common.PageDefinition;
+import pl.training.blog.common.ResultPage;
+import pl.training.blog.domain.Article;
+import pl.training.blog.domain.ArticleCategory;
+import pl.training.blog.domain.Tag;
+import pl.training.blog.ports.infrastructure.ArticleRepository;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@Primary
+@Transactional
+@Repository
+@RequiredArgsConstructor
+public class SpringDataRepositoryAdapter implements ArticleRepository {
+
+    private final SpringDataArticleRepository articleRepository;
+    private final SpringDataArticleMapper mapper;
+
+    @Override
+    public Optional<Article> findById(UUID id) {
+        return articleRepository.findById(id)
+                .map(mapper::toDomain);
+    }
+
+    @Override
+    public Article save(Article article) {
+        var articleEntity = mapper.toEntity(article);
+        var savedArticleEntity = articleRepository.save(articleEntity);
+        return mapper.toDomain(savedArticleEntity);
+    }
+
+    @Override
+    public ResultPage<ArticleView> findByCategory(ArticleCategory category, PageDefinition pageDefinition) {
+        var page = mapper.toEntity(pageDefinition);
+        var categoryName = mapper.toEntity(category);
+        var result = articleRepository.findByCategory(categoryName, page);
+        return mapper.toDomain(result);
+    }
+
+    @Override
+    public ResultPage<ArticleView> findByTags(Set<Tag> tags, PageDefinition pageDefinition) {
+        var page = mapper.toEntity(pageDefinition);
+        var tagNames = mapper.toEntity(tags);
+        var result = articleRepository.findByTags(tagNames, tagNames.size(), page);
+        return mapper.toDomain(result);
+    }
+
+    @Override
+    public ResultPage<ArticleView> findByCategoryAndTags(ArticleCategory category, Set<Tag> tags, PageDefinition pageDefinition) {
+        var page = mapper.toEntity(pageDefinition);
+        var categoryName = mapper.toEntity(category);
+        var tagNames = mapper.toEntity(tags);
+        var result = articleRepository.findByCategoryAndTags(categoryName, tagNames, tagNames.size(), page);
+        return mapper.toDomain(result);
+    }
+
+    @Override
+    public void deleteById(UUID articleId) {
+        articleRepository.deleteById(articleId);
+    }
+
+    @Override
+    public boolean existsByTitle(String title) {
+        return articleRepository.existsByTitle(title);
+    }
+
+}
