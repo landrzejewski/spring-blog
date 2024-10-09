9e1b2833e7ff51da33be0303683560ac79493984
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-10-09 15:27:25 +0200

    x

--- src/main/java/pl/training/blog/BlogApplication.java
+++ src/main/java/pl/training/blog/BlogApplication.java
@@ -1,13 +1,28 @@
 package pl.training.blog;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Predicate;
+
 @SpringBootApplication
-public class BlogApplication {
+@RequiredArgsConstructor
+public class BlogApplication implements ApplicationRunner {
+
+    private final Map<String, Predicate<String>> predicateMap;
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println();
     }
 
 }
