a9b363349ef7f13d4858d9cba74fb85dc67ac504
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-10-09 12:11:47 +0200

    x

--- src/main/java/pl/training/blog/adapters/infrastructure/rest/ArticleTemplateDto.java
+++ src/main/java/pl/training/blog/adapters/infrastructure/rest/ArticleTemplateDto.java
@@ -1,12 +1,10 @@
 package pl.training.blog.adapters.infrastructure.rest;
 
 import lombok.Data;
-import pl.training.blog.common.validation.Condition;
 
 @Data
 public class ArticleTemplateDto {
 
-    @Condition({"uniqueTitle", "minLength"})
     private String title;
     private String author;
     private String content;
