0ae09044f76d61ce52d3058d78c63fa263001409
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-10-10 11:49:17 +0200

    x

---
+++ src/main/java/pl/training/blog/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package pl.training.blog;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        var user = User.withUsername("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(withDefaults())
+                .authorizeHttpRequests(config -> config
+                        .requestMatchers(GET, "/articles/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(GET, "/categories/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .build();
+    }
+
+}
