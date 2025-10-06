548dfb93763a2fd0e8a8038628b2f47cce944fba
Admin <landrzejewski.poczta@gmail.com>
2025-10-06 12:31:30 +0200

    update

--- src/main/java/pl/training/blog/BlogApplication.java
+++ src/main/java/pl/training/blog/BlogApplication.java
@@ -2,6 +2,7 @@ package pl.training.blog;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pl.training.blog.application.ArticleTemplate;
 import pl.training.blog.ports.api.ArticleAuthorActionsApi;
 import pl.training.blog.ports.api.ArticleSearchApi;
@@ -13,12 +14,14 @@ import static pl.training.blog.domain.ArticleCategory.IT;
 public class BlogApplication {
 
     public static void main(String[] args) {
-            ArticleAuthorActionsApi authorActions = null;
-            ArticleSearchApi search = null;
+        try (var container = new AnnotationConfigApplicationContext(BlogConfiguration.class)) {
+            var authorActions = container.getBean(ArticleAuthorActionsApi.class);
+            var search = container.getBean(ArticleSearchApi.class);
 
-            var article = new ArticleTemplate("Test", "Jan Kowalski", "",  IT);
+            var article = new ArticleTemplate("Test", "Jan Kowalski", "", IT);
             var id = authorActions.create(article);
             log.info(search.findByUid(id).toString());
-     }
+        }
+    }
 
 }
