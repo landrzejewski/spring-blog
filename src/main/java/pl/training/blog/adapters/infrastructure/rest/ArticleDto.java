2a1c176464509df1d23dc9fc58a65f960ff8ffd1
Admin <landrzejewski.poczta@gmail.com>
2026-05-19 14:47:32 +0200

    update

---
+++ src/main/java/pl/training/blog/adapters/infrastructure/rest/ArticleDto.java
@@ -0,0 +1,19 @@
+package pl.training.blog.adapters.rest;
+
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+public class ArticleDto {
+
+    private String title;
+    private String author;
+    private String content;
+    private String category;
+    private String status;
+    private Instant created;
+    private int likes;
+    private int dislikes;
+
+}
